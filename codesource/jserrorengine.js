d486bad9a75973deecf5837a3cea5a17e7c070df
ST5Sneaky <105569073+ST5Sneaky@users.noreply.github.com>
2022-06-13 20:02:11 -0400

    error 404

---
+++ codesource/jserrorengine.js
@@ -0,0 +1,35 @@
+function errorDetected() {
+  var funval = Math.floor(Math.random() * 10);
+  if (funval = 1) {
+   alert("Error detected!")
+  }
+  if (funval = 2) {
+   alert("THIS IS AN ERROR MESSAGE!")
+  }
+  if (funval = 3) {
+   alert("error")
+  }
+  if (funval = 4) {
+   alert("y u encounter an error?")
+  }
+  if (funval = 5) {
+   alert("ERRRRRRRRRRRRRRRRRRRRRROR")
+  }
+  if (funval = 6) {
+   alert("This is an error message! REALLY! I'M SERIOUS!")
+  }
+  if (funval = 7) {
+   alert("Thanks for playing the game ERROR!")
+  }
+  if (funval = 8) {
+   alert("Hmm, looks like you made an error appear!")
+  }
+  if (funval = 9) {
+   alert("ERROR 404")
+  }
+  if (funval = 10) {
+   alert("A wild ERROR appeared!")
+  }
+}
+  
+  
